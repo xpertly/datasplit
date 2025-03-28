450dd8eab242c97a765076820340c87a99a52de3
Ajoy <1134055+aj0y@users.noreply.github.com>
2025-03-28 11:45:53 -0700

    Create split.js

---
+++ split.js
@@ -0,0 +1,24 @@
+// read in the data from data.json
+
+// given a MAX json chunk size in bytes,
+// split the json in data.json into smaller chunks of valid json
+const MAX_CHUNK_SIZE = 10000; // bytes
+
+// each chunk should contain the structure specified in "chunk_template":
+const chunk_template =  {
+	"Actions": {
+		"comments": [],
+    "posts": [],
+    "messages": [],
+    "reactions": [],
+    "lastFetchTimestamp": 123456789
+  },
+  "Profile": {
+    "recentConnections": [],
+    "recentFollowers": [],
+  },
+  "retry_count": 0,
+  "guuid": "ABCD1234"
+};
+
+
